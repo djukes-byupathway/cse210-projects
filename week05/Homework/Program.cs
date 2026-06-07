480fef59e20a5f54d6b38e9fa15a2a01f24af172
djukes-byupathway <djukes@byupathway.edu>
2026-06-07 09:43:24 -0500

    week 5 homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine();
+        Assignment myAssignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(myAssignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment myAssignment2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(myAssignment2.GetSummary());
+        Console.WriteLine(myAssignment2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment myAssignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(myAssignment3.GetSummary());
+        Console.WriteLine(myAssignment3.GetWritingInfo());
+        Console.WriteLine();
+
     }
 }
